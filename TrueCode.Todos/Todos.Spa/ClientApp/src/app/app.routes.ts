86b7e2c140fc904ccce57f7aaa19f24ea2031d76
Lashkevich Ryan <lashkevichrain@gmail.com>
2024-07-12 11:36:50 +0700

    wip(frontend): redirect

--- TrueCode.Todos/Todos.Spa/ClientApp/src/app/app.routes.ts
+++ TrueCode.Todos/Todos.Spa/ClientApp/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { LoginComponent } from './components/login/login.component';
 import { AuthGuardService } from './services/auth-guard.service';
 
 export const routes: Routes = [
+  { path: '', redirectTo: "todos", pathMatch: 'full' },
   { path: 'first', component: FirstComponent },
   { path: 'second', component: SecondComponent },
   { path: 'todos', component: TodosComponent, canActivate: [AuthGuardService] },
--- TrueCode.Todos/Todos.Spa/ClientApp/src/app/components/todos/todos.component.ts
+++ TrueCode.Todos/Todos.Spa/ClientApp/src/app/components/todos/todos.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { ApiUrls } from '../../app.config';
+import { HttpClient } from '@angular/common/http';
+import { MatButtonModule } from '@angular/material/button';
+import { catchError, map } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './todos.component.html',
-  styleUrl: './todos.component.css'
+  styleUrl: './todos.component.css',
 })
 export class TodosComponent {
+  constructor(private httpClient: HttpClient) {}
 
+  authTest() {
+    this.httpClient
+      .get(ApiUrls.Base + '/api/todos/getuser')
+      .subscribe((r) => console.log(r));
+  }
 }

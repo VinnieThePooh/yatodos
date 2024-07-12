d72b1dc2555ecc21f805e6f35bae6c336affe59a
Lashkevich Ryan <lashkevichrain@gmail.com>
2024-07-12 08:47:51 +0700

    refactor(): seeding data

--- TrueCode.Todos/Todos.Api/SampleData.cs
+++ TrueCode.Todos/Todos.Api/SampleData.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Todos.DataAccess;
 using Todos.DataAccess.Identity;
 using Todos.Models.Domain;
@@ -11,48 +12,63 @@ public static class SampleData
     public static async Task SeedUsersAndRoles(IServiceProvider serviceProvider)
     {
         var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
-        
-        string[] rolesNames = ["Administrator", "User"];
-        List<AppRole> roles = new();
         var context = serviceProvider.GetRequiredService<TodosContext>();
         var roleStore = new RoleStore<AppRole, TodosContext, int>(context);
-
-        //init roles
-        foreach (string roleName in rolesNames)
-        {
-            if (!context.Roles.Any(r => r.Name == roleName))
-            {
-                var role = new AppRole { Name = roleName, NormalizedName = userManager.KeyNormalizer.NormalizeName(roleName) };
-                roles.Add(role);
-                await roleStore.CreateAsync(role);
-            }
-        }
+        
+        string[] rolesNames = ["Administrator", "User"];
+        
+        var allRoles = await context.Roles.ToArrayAsync();
+        await InitRoles(rolesNames, roleStore, userManager, allRoles);
         
         //init users
-        var name = "ValeraAdmin";
-        var email = "valeraadmin@ryansoftware.com";
-        var password = "secret";
+        var users = new [] {
         
-        var user = new AppUser
-        {
-            Email = email,
-            NormalizedEmail =  userManager.KeyNormalizer.NormalizeEmail(email),
-            UserName = name,
-            NormalizedUserName = userManager.KeyNormalizer.NormalizeName(name),
-            PhoneNumber = "+111111111111",
-            EmailConfirmed = true,
-            PhoneNumberConfirmed = true,
-            SecurityStamp = Guid.NewGuid().ToString("D"),
+            ("ValeraAdmin", "valeraadmin@ryansoftware.com", "secret"),
+            ("NarutoUzumaki", "uzumaki@ryansoftware.com", "narutoSecret")
         };
-        
-        if (!context.Users.Any(u => u.UserName == user.UserName))
+        await InitUsers(users, allRoles, userManager);
+    }
+
+    private static async Task InitRoles(string[] roleNames, IRoleStore<AppRole> roleStore, UserManager<AppUser> userManager, AppRole[] allRoles)
+    {
+        foreach (string roleName in roleNames)
         {
-            user.PasswordHash = userManager.PasswordHasher.HashPassword(user, password);
-            //our domain part
-            user.User = new User { UserName = user.UserName };
-            _ = await userManager.CreateAsync(user);
+            if (!allRoles.Any(r => r.Name == roleName))
+            {
+                var role = new AppRole { Name = roleName, NormalizedName = userManager.KeyNormalizer.NormalizeName(roleName) };
+                await roleStore.CreateAsync(role, CancellationToken.None);
+            }
         }
-        await AssignRoles(userManager, user, roles);
+    }
+
+    private static async Task InitUsers(
+        (string name, string email, string password)[] users, 
+        AppRole[] allRoles,
+        UserManager<AppUser> userManager)
+    {
+        foreach (var (name, email, password) in users)
+        {
+            var user = new AppUser
+            {
+                Email = email,
+                NormalizedEmail =  userManager.KeyNormalizer.NormalizeEmail(email),
+                UserName = name,
+                NormalizedUserName = userManager.KeyNormalizer.NormalizeName(name),
+                PhoneNumber = "+111111111111",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                SecurityStamp = Guid.NewGuid().ToString("D"),
+            };
+            
+            if (!userManager.Users.Any(u => u.UserName == user.UserName))
+            {
+                user.PasswordHash = userManager.PasswordHasher.HashPassword(user, password);
+                //our domain part
+                user.User = new User { UserName = user.UserName };
+                _ = await userManager.CreateAsync(user);
+            }
+            await AssignRoles(userManager, user, allRoles);
+        }
     }
 
     private static async Task AssignRoles(UserManager<AppUser> userManager, AppUser user, IEnumerable<AppRole> roles)
